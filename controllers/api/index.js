fab1fb426e352dbee42bde90739f8c4a9a00afe0
Jobi Mathai <jobi.mathai@gmail.com>
2019-01-21 15:03:29 -0800

    fixed issue

---
+++ controllers/api/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+
+    router.get('/', function(req, res) {
+        res.status(200).send('<a href=\'/api/articles/\'>articles</a><br><a href=\'/api/notes/\'>notes</a>');
+    });
+
+router.use('/articles', require('./articles'));
+router.use('/notes', require('./notes'));
+
+module.exports = router;
