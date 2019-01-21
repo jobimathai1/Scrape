f7598e1f44b70ce65421d7567afc93066c6bf657
Jobi Mathai <jobi.mathai@gmail.com>
2019-01-21 13:05:13 -0800

    server.js

---
+++ server.js
@@ -0,0 +1,36 @@
+'use strict';
+var express = require('express');
+var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var PORT = process.env.PORT || 3000;
+var app = express();
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.text())
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }))
+app.use(logger('dev'))
+app.use(express.static(__dirname + '/public'))
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(require('./controllers'));
+
+mongoose.Promise = Promise;
+
+var dbURI = process.env.MONGODB_URI || "mongodb://localhost:27017/news";
+
+mongoose.connect(dbURI, { useNewUrlParser: true });
+mongoose.set('useCreateIndex', true);
+
+var db = mongoose.connection;
+
+db.on("open", function () {
+    console.log("Mongoose connection successful.");
+    app.listen(PORT, function () {
+        console.log("App running on port:  " + PORT);
+    });
+});
+
+module.exports = app;
